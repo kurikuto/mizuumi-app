7d3fab26797181f660e945db45effe1178f827e6
kurikuto <k.ab.kurikuto@gmail.com>
2020-01-25 16:43:15 +0900

    login画面 仮デザイン

--- js/src/components/Login/Login.js
+++ js/src/components/Login/Login.js
@@ -8,17 +8,20 @@ const Login = (props) => {
   }
 
   return (
-    <div>
-      <p>Login</p>
-      <div>
-        <span>user</span>
-        <input type="text" />
+    <div class="login">
+      <h2 id="title">Your Favorite Movies.</h2>
+      <div class="content">
+        <p>Welcome</p>
+        <div>
+          <span>user: </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>password: </span>
+          <input type="text" />
+        </div>
+        <button class="button" onClick={authLogin}>login</button>
       </div>
-      <div>
-        <span>password</span>
-        <input type="text" />
-      </div>
-      <button onClick={authLogin}>login</button>
     </div>
   );
 }
